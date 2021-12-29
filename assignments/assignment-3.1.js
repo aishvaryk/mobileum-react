f6d0b288dd0161ebe8ef14bfbb87ffa38da7c36f
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2021-12-29 10:38:02 +0530

    refactored

--- assignments/assignment-3.1.js
+++ assignments/assignment-3.1.js
@@ -1,41 +1,43 @@
-function Person (name, age) {
+function Person(name, age) {
     this.name = name;
     this.age = age;
     this.license = [];
 }
 
-Person.prototype.teachDrive = function (vehicleType) {
-    if (this.age <18) {
+teachDrive = function (person, vehicleType) {
+    if (person.age < 18) {
         console.log("Age must be at least 18");
     }
     else {
-        console.log(this.name + " can drive " + vehicleType);
-        this.issueLicense(vehicleType);
+        console.log(person.name + " can drive " + vehicleType);
+        issueLicense(person, vehicleType);
     }
 }
 
-Person.prototype.issueLicense = function (vehicleType) {
-    if (this.age <18) {
+Drive = function drive(vehicleType) {
+    if (this.license.includes(vehicleType)) {
+        console.log(this.name + " is driving " + vehicleType)
+    }
+    else {
+        console.log(this.name + "needs a license to drive " + vehicleType)
+    }
+}
+
+issueLicense = function (person, vehicleType) {
+    if (person.age < 18) {
         console.log("Age must be at least 18");
     }
     else {
-        this.license.push(vehicleType);
-        if (this.license.length==1) {
-            this.drive = function(vehicleType) {
-                if (this.license.includes(vehicleType)) {
-                    console.log(this.name + " is driving " + vehicleType)
-                }
-                else {
-                    console.log(this.name + "needs a license to drive " + vehicleType)
-                }
-            }
-            }
+        person.license.push(vehicleType);
+        if (person.license.length == 1) {
+            person.drive = Drive;
+        }
     }
 }
 var prabhat = new Person("Prabhat", 40);
 var shivanshi = new Person("Shivanshi", 15);
 
-prabhat.teachDrive("car");
+teachDrive(prabhat, "car");
 console.log(prabhat.license);
 prabhat.drive("car");
 prabhat.drive("truck");
