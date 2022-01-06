a57b17b2b7231f9f84fbd90f0dbe906d16b569ee
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2022-01-06 15:39:56 +0530

    submitted assignent 9

--- assignments/assignment -9/app.js
+++ assignments/assignment -9/app.js
@@ -1,34 +1,31 @@
-const express = require('express')
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-const books = fs.readFileSync('books5.json')
+const books = fs.readFileSync("books5.json");
 
-const app = express()
+const app = express();
 
-const port = 5001
+const port = 5001;
 
-app.get('/', (req, res) => {
-    res.redirect("/api/books")
-})
+app.get("/", (req, res) => {
+  res.redirect("/api/books");
+});
 
-app.get('/api/books', (req, res) => {
-    res.send(books.toString())
-})
+app.get("/api/books", (req, res) => {
+  res.send(books.toString());
+});
 
-app.post('/api/books', (req, res) => {
-    res.send('Work in progress')
-})
+app.post("/api/books", (req, res) => {
+  res.send("Work in progress");
+});
 
-app.get('/api/books/:isbn', (req, res) => {
-    var selectedbook = books.filter(b => b.isbn == req.params.isbn)
-    res.send(selectedbook.toString())
-})
+app.get("/api/books/:isbn", (req, res) => {
+  var selectedbook = books.filter((b) => b.isbn == req.params.isbn);
+  res.send(selectedbook.toString());
+});
 
+const server = app.listen(port, (_) => {
+  console.log("Server started on port: ", port);
+});
 
-
-
-const server = app.listen(port,_ => {
-    console.log("Server started on port: ", port)
-})
-
-server.on("error", error => console.error('error', error.message))
+server.on("error", (error) => console.error("error", error.message));
